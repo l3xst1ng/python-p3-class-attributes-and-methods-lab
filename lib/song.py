732306af2e354e31aa85ac070cc470abbc0117ef
l3xst1ng <howdy.martt@gmail.com>
2024-05-30 17:47:36 +0300

    Class Attributes and Methods Lab

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,67 @@
+
 class Song:
-    pass
+    # Class attributes
+    
+    # Keeping track of the total count of songs
+    count = 0
+    
+    #  storing  unique genres,artists
+    genres = []
+    artists = []
+    
+    #  attribute to store the count of songs for each genre
+    genre_count = {}
+    
+    # attribute to store the count of songs for each artist
+    artist_count = {}
+    
+    def __init__(self, name, artist, genre):
+        # Instance attribute 
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        
+        # Incrementing the total count of songs
+        Song.count += 1
+        
+        # Adding the genre to the list of unique genres
+        Song.add_to_genres(genre)
+        
+        # Add an artist to the list of unique artists
+        Song.add_to_artists(artist)
+        
+        # Updating the count of songs for the genre
+        Song.add_to_genre_count(genre)
+        
+        # Updating the count of songs for the artist
+        Song.add_to_artist_count(artist)
+    
+    @classmethod
+    def add_to_genres(cls, genre):
+        # Checking if the genre is not already present in the genres list
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+    
+    @classmethod
+    def add_to_artists(cls, artist):
+        # Check if the artist is not already present in the artists list
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+    
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        # Check if the genre already exists as a key in the genre_count dictionary
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            # Add a new key-value pair with the genre as the key set to 1 as the initial value
+            cls.genre_count[genre] = 1
+    
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        # Check if the artist already exists as a key in the artist_count dictionary
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
+        else:
+            # Add a new key-value pair with the artist as the key and 1 as the initial value
+            cls.artist_count[artist] = 1
